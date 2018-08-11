49cdc331ba8cc112cce69b046ed2f97f1c6659e9
shullmb <shullmb@gmail.com>
2018-08-11 10:49:12 -0700

    frontend/model testing

--- client/src/WelcomeText.jsx
+++ client/src/WelcomeText.jsx
@@ -5,7 +5,7 @@ export const WelcomeText = props => {
   return (
     <div className="dash-box">
       <h4>{props.user.name},</h4>
-      <p>We are so glad that you have chosen <strong>Optiplot</strong> to help manage your gardens!</p>
+      <p>We are so glad that you have chosen <span className="text-plot">Optiplot</span> to help manage your gardens!</p>
       <p>Here are some of the things we can help you do:</p>
       <p>&nbsp; &nbsp; &bull; &nbsp; Find growing information for a variety of crops</p>
       <p>&nbsp; &nbsp; &bull; &nbsp; Choose the best crops to combine in your plot</p>
