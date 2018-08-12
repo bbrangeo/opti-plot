715142529c58b04b1acb71d7ed88f70bcaddf1db
shullmb <shullmb@gmail.com>
2018-08-12 14:15:21 -0700

    cleaning up debug

--- client/src/DashList.jsx
+++ client/src/DashList.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { List, ListSubheader, ListItem, ListItemText } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
@@ -24,44 +24,34 @@ const styles = theme => ({
   },
 });
 
-class DashList extends Component  {
-  constructor(props) {
-    super(props)
-    this.state = {
-      gardens: this.props.user.gardens
-    }
-  }
+const DashList = props =>  {
+  const { classes } = props;
 
-  render() {
-    const { classes } = this.props;
-    let datetime = new Date();
-    console.log(datetime,"====>",this.props.user)
-    const list = this.state.gardens[0].hasOwnProperty('plots') ? 
-        <List className={classes.root} subheader={<li />}>
-          {this.state.gardens.map(garden => (
-            <li key={`section-${garden._id}`} className={classes.listSection}>
-              <ul className={classes.ul}>
-                <ListSubheader><Link to={`/gardens/${garden._id}`} className="link-btn">{garden.name}</Link></ListSubheader>
-                { 
-                  garden.plots.map( plot => (
-                    <ListItem key={`item-${garden.name}-${plot.name}`} className={classes.ListItem} >
-                      <ListItemText primary={plot.name} />
-                    </ListItem>
-                   ))
-                }
-              </ul>
-            </li>
-          ))}
-        </List>
-     : <p>no gardens yet</p>;
-    
-    return (
-      <div className="dash-box">
-        <h4>My Gardens</h4>
-          {list}
-      </div>
-    )
-  }
+  const list = props.user.gardens.length > 0 ? 
+      <List className={classes.root} subheader={<li />}>
+        {props.user.gardens.map(garden => (
+          <li key={`section-${garden._id}`} className={classes.listSection}>
+            <ul className={classes.ul}>
+              <ListSubheader><Link to={`/gardens/${garden._id}`} className="link-btn">{garden.name}</Link></ListSubheader>
+              { 
+                garden.plots.map( plot => (
+                  <ListItem key={`item-${garden.name}-${plot.name}`} className={classes.ListItem} >
+                    <ListItemText primary={plot.name} />
+                  </ListItem>
+                  ))
+              }
+            </ul>
+          </li>
+        ))}
+      </List>
+    : <p>no gardens yet</p>;
+  
+  return (
+    <div className="dash-box">
+      <h4>My Gardens</h4>
+        {list}
+    </div>
+  )
 }
 
 export default withStyles(styles)(DashList);
