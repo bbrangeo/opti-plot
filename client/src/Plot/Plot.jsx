902fc7c89c48ea3e82a3b6b4b5721021d0a9f6cc
shullmb <shullmb@gmail.com>
2018-08-13 14:04:55 -0700

    debugging plot show

--- client/src/Plot/Plot.jsx
+++ client/src/Plot/Plot.jsx
@@ -7,28 +7,33 @@ export class Plot extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      plot: this.props.plot
+      plot: null
     }
   }
 
-  // componentDidMount() {
-  //   axios.get(`/plots/${this.props.plot}`)
-  //     .then( response => {
-  //       this.setState({
-  //         plot: response.data
-  //       })
-  //     }).catch( err => console.log(err))
-  // }
+  componentDidMount() {
+    axios.get(`/plots/${this.props.plot._id}`)
+      .then( response => {
+        this.setState({
+          plot: response.data
+        })
+      }).catch( err => console.log(err))
+  }
   
   render() {
-    const plot = this.state.plot ? this.state.plot : ''
-    const crops = this.state.plot.crops ? this.state.crops : ''
-
-
-    return(
+    const plot = this.state.plot ? this.state.plot : null
+    const crops = this.state.plot.hasOwnProperty('crops') ? this.state.plot.crops.map(crop => {
+      return (
+        <div>
+          <h4>{crop.name}</h4>
+          <p>{crop.sunRequirements}</p>
+        </div>
+      )
+      }) : ''
+    const display = this.state.plot ? (
       <div class="dash-box">
         <Grid container spacing={16} >
-        <h1>{plot.name}</h1>
+          <h1>{plot.name}</h1>
           <Grid item xs={12}>
             <h3>Plot Specs</h3>
             <h4>{plot.season}</h4>
@@ -36,29 +41,19 @@ export class Plot extends Component {
           </Grid>
           <Grid item xs={12} md={6}>
             <h3>Optimized Crop Configuration</h3>
-            {
-              crops ? crops.map( crop => {
-                return (
-                  <div>
-                    <h4>{crop.name}</h4>
-                    <p>{crop.sunRequirements}</p>
-                  </div>
-
-                )
-              }) : ''
-            }
+            {crops}
           </Grid>
           <Grid item xs={12} md={6} className='plot-container'>
-            <div className="display-plot" style={{width: plot.length * 10 + '%', height: plot.width * 10 + '%',}}>
-              {
-                crops ? crops.map( crop => {
-                  return <Icon src={crop.icon} size={20} />
-                }) : plot.width*(<p>&nbsp;</p>)
-              }
+            <div className="display-plot" style={{ width: plot.length * 10 + '%', height: plot.width * 10 + '%', }}>
             </div>
           </Grid>
         </Grid>
       </div>
+        ) : '' ;
+
+
+    return(
+      {display}
     )
   }
 }
