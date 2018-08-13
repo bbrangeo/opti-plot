d01296a6936b15ccf310dfc701bd65aa5ae90a19
shullmb <shullmb@gmail.com>
2018-08-13 10:51:03 -0700

    gardens summary styling

--- client/src/Garden/Garden.jsx
+++ client/src/Garden/Garden.jsx
@@ -36,25 +36,30 @@ export const Garden = props => {
       })
   }
 
+  const deleteCropFromGarden = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    let target = e.currentTarget.href
+    axios.delete(target)
+      .then(response => {
+        console.log(response)
+        props.updateUser();
+      })
+  }
+
   return (
     <div className="dash-box">
       <Grid container spacing={16}>
         <Grid item xs={12}>
-          <h1>{garden.name}</h1><Button variant="contained" onClick={(e) => {deleteGarden(e)}} color="secondary" >DELETE</Button>
+          <h1>{garden.name}&nbsp;&nbsp;<Button variant="contained" onClick={(e) => { deleteGarden(e) }} color="secondary" className="del-btn"><DeleteIcon />Remove</Button></h1>
 
         </Grid>
-        <Grid container spacing={16} justify="center" alignContent="center" alignItems="center">
-          <Grid item xs={12} md={3} className="dash-box">
-            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-              {
-                garden.cropsChosen.map(crop => <Icon src={crop.icon} size={30} />)
-              }
-            </div>
-          </Grid>
-          <Grid item xs={12} md={3} className="dash-box">
+        <Grid container spacing={16} justifyContent="space-between">
+          <Grid item xs={12} md={4} className="dash-box">
+            <h3>Plots</h3>
             <ul>
               {
-                garden.plots.map(plot => <li key={plot._id}>{plot.name} 
+                garden.plots.map(plot => <li key={plot._id}>{plot.name}&nbsp;&nbsp; 
                                           <Button href={`/plots/${plot._id}`} 
                                             onClick={(e) => deletePlot(e)} 
                                             variant="fab"
@@ -63,16 +68,25 @@ export const Garden = props => {
                                             >
                                             <DeleteIcon />
                                           </Button>
-                                         </li>)
+                                         </li>
+                                        )
               }
             </ul>
           </Grid>
-          <Grid item xs={12} md={3} className="dash-box">
+          <Grid item xs={12} md={4} className="dash-box">
+            <h3>Crops Chosen</h3>
             <ul>
               {
                 garden.cropsChosen.map(crop => <li key={crop.ofId}>{crop.name}</li>)
               }
             </ul>
+          </Grid>
+          <Grid item xs={12} md={4} className="dash-box">
+            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+              {
+                garden.cropsChosen.map(crop => <Icon src={crop.icon} size={30} />)
+              }
+            </div>
           </Grid>
         </Grid>
       </Grid>
